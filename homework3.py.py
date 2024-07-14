b1be06a72394c850da5ff45cbf290aa03099b18c
AndreiT80 <and8016t@gmail.com>
2024-07-14 21:52:54 +0700

    Add files via upload

---
+++ homework3.py.py
@@ -0,0 +1,8 @@
+name=('Andrei')
+print(name)
+age=59             # My age
+print(age)
+age=(59+5)         # How old will I be in five years
+print(age)
+is_student=(9<12)  # Study time is 9 months less than a year
+print(is_student)
